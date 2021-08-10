7166d5e1396a4aeb1a59f191937dc914862a7660
guillaumeslash <ytbgui@gmail.com>
2021-08-10 11:34:03 +0200

    working timer

---
+++ script.js
@@ -0,0 +1,31 @@
+let timer = 600;
+
+document.getElementById("timer").innerHTML = "Lezgongue";
+
+const interval = setInterval(function() {
+   if (timer > 0) {
+      timer--;
+      document.getElementById("timer").innerHTML = toTime(timer);
+   }
+   else {
+      clearInterval(interval);
+   }
+}, 1000);
+
+
+const addMins = (mins) => {
+  timer = timer + 60*mins;
+}
+
+const removeMins = (mins) => {
+  if (timer > 60) {
+    timer = timer - 60*mins;
+  }
+
+}
+
+const toTime = (seconds) => {
+   var date = new Date(null);
+   date.setSeconds(seconds);
+   return date.toISOString().substr(11, 8);
+}

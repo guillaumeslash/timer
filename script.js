6299a20ba4cb2ac5cf02ac3599461b117d9aefc0
guillaumeslash <ytbgui@gmail.com>
2021-09-23 11:01:05 +0200

    if 7am then remaining time to 8am

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// Do calculation of remaining seconds before 8am only between 7am and 8am everyday
+
+// get current time
+const currentTime = new Date();
 let timer = 600;
 
-document.getElementById("timer").innerHTML = "Lezgongue";
+// If it's 7:XX am
+if (currentTime.getHours() === 7) {
+  timer = ((60 - currentTime.getMinutes()) * 60) - currentTime.getSeconds();
+}
+
+document.getElementById("timer").innerHTML = "XX:XX";
 
 const interval = setInterval(function() {
    if (timer > 0) {
